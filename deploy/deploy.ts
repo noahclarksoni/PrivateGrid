34a0ea560fa08d675fdeb0c9319629f94651ea89
noahclarksoni <eqhqvpv1116@outlook.com>
2025-12-25 16:03:05 +0800

    Add DocumentVault deploy script

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedDocumentVault = await deploy("DocumentVault", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`DocumentVault contract: `, deployedDocumentVault.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_documentVault"; // id required to prevent reexecution
+func.tags = ["DocumentVault"];
